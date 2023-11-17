387fe0db1b46df6c281d679dbf77a2f3e2c9a63f
yamaceay <yamacerenay2001@gmail.com>
2023-11-17 16:44:13 +0100

    refactor

--- src/players.py
+++ src/players.py
@@ -62,75 +62,6 @@ class UCTPlayer(Player):
         best_child = max(tree.root.children, key=lambda child: child.Q)
         return best_child.state.history[-1]
     
-class Model:
-    def __call__(self, state: Gomoku) -> float:
-        raise NotImplementedError
-    
-class ADPModel(Model):
-    def __call__(self, state: Gomoku) -> float:
-        return np.random.random()
-
-line_to_int = lambda k: [1 if c == 'x' else -1 if c == 'o' else 0 for c in k]
-int_to_line = lambda k: ''.join(['x' if c == 1 else 'o' if c == -1 else '-' for c in k])   
-    
-def pb_score(parent: Node, child: Node, **kwargs) -> float:
-    decay = kwargs.get('decay', 0.9)
-    PB_DICT = {
-        '-oooo-': 10000,
-        'x-ooo-x': 1000*decay**3,
-        'x-ooo--': 1000*decay**1,
-        '--ooo--': 1000,
-        '--ooo-o': 1000,
-        'o-ooo-o': 10000,
-        'x-ooo-o': 1000*decay**1,
-        '--oo--': 100,
-        'x-oo--': 100*decay**1,
-        'xoo-o--': 100*decay**2,
-        'xo-oo--': 100*decay**2,
-        'xo-oo-x': 100*decay**3,
-        'xoo-o-x': 100*decay**3,
-        'xooo--':  100,
-        'xoo---': 1,
-        'xoooo-': 1000,
-        '-oo-o-': 1000*decay**1,
-        'xooo-o-': 1000*decay**1,
-        'xoo-oo-': 1000*decay**1,
-        'xooo-ox': 1000*decay**3,
-        'xoo-oox': 1000*decay**3,
-        'xooo-oo': 1000*decay**1,
-        '-o-o-o-': 1000*decay**2,
-        'xo-o-ox': 1000*decay**6,
-        'xo-o-o-': 1000*decay**4,
-        '--o-o--': 100*decay**2,
-        'x-o-o--': 100*decay**4,
-        'x-o-o-x': 100*decay**6,
-        '--o--': 1,
-    }
-    
-    move = child.state.history[-1]
-    pb_value = 0
-    for pattern, value in PB_DICT.items():
-        values = line_to_int(pattern)
-        pb_parent = parent.state.find_near(move, values)
-        pb_child = child.state.find_near(move, values)
-        pb_diff = (pb_child - pb_parent) * value
-        
-        reverse_values = [-c for c in values]
-        pb_reverse_parent = parent.state.find_near(move, reverse_values)
-        pb_reverse_child = child.state.find_near(move, reverse_values)
-        pb_reverse_diff = (pb_reverse_child - pb_reverse_parent) * (-5 * value)
-        
-        pb_value += pb_diff + pb_reverse_diff
-    pb_value *= child.state.player
-    pb_value /= 100000
-    return pb_value
-    
-def uct_pb_score(parent: Node, child: Node, **kwargs) -> float:
-    C_PB = kwargs.get('C_PB', 5)
-    ucb = uct_score(parent, child, **kwargs)
-    pbs = pb_score(parent, child, **kwargs)
-    return ucb + C_PB * pbs
-    
 class UCTQPlayer(Player):
     def __init__(self, iterations=1000, max_depth=10, policy=uct_score, policy_kwargs={}, tree_kwargs={}, model=None):
         self.iterations = iterations
@@ -173,3 +104,11 @@ class UCTQPlayer(Player):
         
         best_child = max(tree.root.children, key=lambda child: child.Q)
         return best_child.state.history[-1]
+    
+class Model:
+    def __call__(self, state: Gomoku) -> float:
+        raise NotImplementedError
+    
+class ADPModel(Model):
+    def __call__(self, state: Gomoku) -> float:
+        return np.random.random()

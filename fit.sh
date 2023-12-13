9d225c235c9a9af4a7bf31ffae6a1c612adc1ccf
yamaceay <yamacerenay2001@gmail.com>
2023-12-13 16:55:34 +0100

    fix

--- fit.sh
+++ fit.sh
@@ -6,7 +6,7 @@
 #SBATCH --output=logs/job-%j
 
 # # testing
-# apptainer run --nv gomoku.sif python -m src.mcts_adp --DIR="_dens" --PLAYER="dense" --EPOCH=250 --EPSILON=.25 --ITERATIONS=200 --MAX_DEPTH=5
+# apptainer run --nv gomoku.sif python -m test --DIR="_dens" --PLAYER="dense" --EPOCH=250 --EPSILON=.25 --ITERATIONS=200 --MAX_DEPTH=5
 
 # # training
 apptainer run --nv gomoku.sif python -m fit --DIR="_dens" --PLAYER="dense" --START=320 --END=500 --STEP=10 --BATCH_SIZE=20 --LR=0.01 --LR_DECAY=0.995 --NO_EVAL --EPSILON=0.25

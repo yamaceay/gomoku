0d9b95c789e464f026da3473c00d4b8a3491f976
yamaceay <yamacerenay2001@gmail.com>
2023-12-17 12:04:30 +0300

    pattern matching

--- src/patterns.py
+++ src/patterns.py
@@ -48,35 +48,35 @@ class Pattern:
         return 'x' if c == 1 else 'o' if c == -1 else '-'
 
 PB_DICT = {
-    '-oooo-': lambda _: 10000,
-    'x-ooo-x': lambda decay: 1000*decay**3,
-    'x-ooo--': lambda decay: 1000*decay**1,
-    '--ooo--': lambda _: 1000,
-    '--ooo-o': lambda _: 1000,
-    'o-ooo-o': lambda _: 10000,
-    'x-ooo-o': lambda decay: 1000*decay**1,
-    '--oo--': lambda _: 100,
-    'x-oo--': lambda decay: 100*decay**1,
-    'xoo-o--': lambda decay: 100*decay**2,
-    'xo-oo--': lambda decay: 100*decay**2,
-    'xo-oo-x': lambda decay: 100*decay**3,
-    'xoo-o-x': lambda decay: 100*decay**3,
-    'xooo--':  lambda _: 100,
-    'xoo---': lambda _: 1,
-    'xoooo-': lambda _: 1000,
-    '-oo-o-': lambda decay: 1000*decay**1,
-    'xooo-o-': lambda decay: 1000*decay**1,
-    'xoo-oo-': lambda decay: 1000*decay**1,
-    'xooo-ox': lambda decay: 1000*decay**3,
-    'xoo-oox': lambda decay: 1000*decay**3,
-    'xooo-oo': lambda decay: 1000*decay**1,
-    '-o-o-o-': lambda decay: 1000*decay**2,
-    'xo-o-ox': lambda decay: 1000*decay**6,
-    'xo-o-o-': lambda decay: 1000*decay**4,
-    '--o-o--': lambda decay: 100*decay**2,
-    'x-o-o--': lambda decay: 100*decay**4,
-    'x-o-o-x': lambda decay: 100*decay**6,
-    '--o--': lambda _: 1,
+    '-oooo-': lambda _: 10 ** 4, # p = 4, d = 0
+    'x-ooo-x': lambda decay: 10 ** 3 * decay ** 3, # p = 3, d = 3  
+    'x-ooo--': lambda decay: 10 ** 3 * decay ** 1, # p = 3, d = 1
+    '--ooo--': lambda _: 10 ** 3, # p = 3, d = 0
+    '--ooo-o': lambda _: 10 ** 3, # p = 3, d = 0
+    'o-ooo-o': lambda _: 10 ** 4, # p = 4, d = 0
+    'x-ooo-o': lambda decay: 10 ** 3 * decay ** 1, # p = 3, d = 1
+    '--oo--': lambda _: 10 ** 2, # p = 2, d = 0
+    'x-oo--': lambda decay: 10 ** 2 * decay ** 1, # p = 2, d = 1
+    'xoo-o--': lambda decay: 10 ** 2 * decay ** 2, # p = 2, d = 2
+    'xo-oo--': lambda decay: 10 ** 2 * decay ** 2, # p = 2, d = 2
+    'xo-oo-x': lambda decay: 10 ** 2 * decay ** 3, # p = 2, d = 3
+    'xoo-o-x': lambda decay: 10 ** 2 * decay ** 3, # p = 2, d = 3
+    'xooo--':  lambda _: 10 ** 2, # p = 2, d = 0
+    'xoo---': lambda _: 10 ** 0, # p = 0, d = 0
+    'xoooo-': lambda _: 10 ** 3, # p = 3, d = 0
+    '-oo-o-': lambda decay: 10 ** 3 * decay ** 1, # p = 3, d = 1
+    'xooo-o-': lambda decay: 10 ** 3 * decay ** 1, # p = 3, d = 1
+    'xoo-oo-': lambda decay: 10 ** 3 * decay ** 1, # p = 3, d = 1
+    'xooo-ox': lambda decay: 10 ** 3 * decay ** 3, # p = 3, d = 3
+    'xoo-oox': lambda decay: 10 ** 3 * decay ** 3, # p = 3, d = 3
+    'xooo-oo': lambda decay: 10 ** 3 * decay ** 1, # p = 3, d = 1
+    '-o-o-o-': lambda decay: 10 ** 3 * decay ** 2, # p = 3, d = 2
+    'xo-o-ox': lambda decay: 10 ** 3 * decay ** 6, # p = 3, d = 6
+    'xo-o-o-': lambda decay: 10 ** 3 * decay ** 4, # p = 3, d = 4
+    '--o-o--': lambda decay: 10 ** 2 * decay ** 2, # p = 2, d = 2
+    'x-o-o--': lambda decay: 10 ** 2 * decay ** 4, # p = 2, d = 4
+    'x-o-o-x': lambda decay: 10 ** 2 * decay ** 6, # p = 2, d = 6
+    '--o--': lambda _: 10 ** 0, # p = 0, d = 0
 }
 
 # WIN_ENCODE = [

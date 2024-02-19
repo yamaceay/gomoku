f3a9644c84a9352675def79ece13b9f679480180
yamaceay <yamacerenay2001@gmail.com>
2024-02-19 07:53:30 +0100

    feat: eval v1

--- fit.sh
+++ fit.sh
@@ -6,7 +6,7 @@
 # #SBATCH --gpus-per-node=1
 
 # training
-apptainer run --nv gomoku.sif python -m src.train
+# apptainer run --nv gomoku.sif python -m src.train -g M
 
 # evaluation
-# apptainer run --nv gomoku.sif python -m src.comp -g L
+apptainer run --nv gomoku.sif python -m src.comp -g L
